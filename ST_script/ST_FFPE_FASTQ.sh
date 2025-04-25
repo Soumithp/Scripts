c32eb91ce58eebd705852ccd73ce5f9fc507bc9c
Soumithp <90527153+Soumithp@users.noreply.github.com>
2025-04-25 11:58:55 -0500

    Add files via upload

---
+++ ST_script/ST_FFPE_FASTQ.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##################################################################################################
+module load spaceranger/1.3.0
+parentDir=/project/SCCC/Hoshida_lab/s226953/ST/
+cd $parentDir
+
+spaceranger mkfastq --id=fastq \
+--run=$parentDir/L002_RGL25a_bcl \
+--csv=$parentDir/spaceranger-simple.csv
